7a93115158325f5e3f5fd200836f80000dd63eb1
Peter Grimm <pagrimm@gmail.com>
2020-06-02 12:12:26 -0700

    add this scope to the click function
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
-  $("p#paragraph1").click(function() {
-    $("p#paragraph1").toggleClass("xyz");
-  });
-  $("p#paragraph2").click(function() {
-    $("p#paragraph2").toggleClass("xyz");
-  });
-  $("p#paragraph3").click(function() {
-    $("p#paragraph3").toggleClass("xyz");
+  $("p").click(function() {
+    $(this).toggleClass("xyz");
   });
 });
 
